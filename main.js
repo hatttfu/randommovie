3f7b9aa87a985422a40df70d08a84bbd753620ec
galiya <gkey-587@mail.ru>
2021-09-29 22:19:55 +0500

    renamed main2.js to main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const API_KEY = 'ce2f8a0c1341cb0cf081ef2e2df670a2';
 const IMG_URL = 'https://image.tmdb.org/t/p/original';
 
+const ids = ['9007', '770635', '4347', '767', '672', '674', '12445', '671', '675', '673', '12444']
+
 //коды для жанров
 
 const genres = { 
@@ -46,6 +48,35 @@ const form = document.querySelector('.form'),
     descriptionM = document.querySelector('.description'),
     releaseM = document.querySelector('.release-date');
 
+let n = 0;
+
+//функция запроса
+function sendRequest(url, region, genre, rating, duration, year) {
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=ru-RU&region=${region}&with_genres=${genre}&vote_count.gte=${rating}&with_runtime.lte=${duration}&release_date.gte=${year}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        randomMovie(data);
+        againBtn.onclick = function() {
+            if(n < 19) {
+                n++;
+                imgRemove();
+                randomMovie(data);
+            } else {
+                n = 0;
+                imgRemove();
+                randomMovie(data);
+                //всплытие напоминалки
+                messageShowUp();
+                messageFadeOut();
+            }      
+        }
+    });
+
+}
+
 form.addEventListener('submit', event => {
     event.preventDefault();
     const genreInput = genreSelect.options[genreSelect.selectedIndex].text;
@@ -55,85 +86,78 @@ form.addEventListener('submit', event => {
     const duration = durationSelect.options[durationSelect.selectedIndex].text;
     const rating = ratingSelect.options[ratingSelect.selectedIndex].text;
 
-    let n = 0;
-
     //запрос
-    async function getResponse() {
-        let response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=ru-RU&region=${region}&with_genres=${genre}&vote_count.gte=${rating}&with_runtime.lte=${duration}&release_date.gte=${year}`)
-        let responseContent = await response.json()
-        console.log(responseContent);
-
-        // randomMovie(responseContent);
-        //random elem from array
-
-            const moviesList = responseContent.results;
-            // const movie = moviesList[Math.ceil(Math.random()*moviesList.length)];
-            movie = moviesList[n];
-            
-        
-        //заполнение 
-        imgAdd(movie.poster_path, movie.backdrop_path);
-        // ImgAdd(movie.backdrop_path);
-        rateM.innerHTML = movie.vote_average;
-        titleM.innerHTML = movie.title;
-        releaseM.innerHTML = movie.release_date;
-        descriptionM.innerHTML = movie.overview;
-        // descriptionM.innerHTML = movie.overview;
-    }
-
-    getResponse().then(modalOpen());
-
-    //при нажатии НЕ ТО
-    againBtn.onclick = function() {
-        if(n < 19) {
-            n++;
-            getResponse().then(imgRemove());
-        } else {
-            n = 0;
-            getResponse().then(imgRemove());
-            //всплытие напоминалки
-        }      
-    }
-
-    exitBtns.forEach( (exitBtn) => {
-        exitBtn.addEventListener( 'click', () => {
-            modalClose();
-            message.classList.add('hide');
-        }) 
-    })
+    sendRequest(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=ru-RU&region=${region}&with_genres=${genre}&vote_count.gte=${rating}&with_runtime.lte=${duration}&release_date.gte=${year}`, region, genre, rating, duration, year);
 
 })
 
-// //random elem from array
-// function randomMovie(responseContent) {
-//     const moviesList = responseContent.results;
-//     // const movie = moviesList[Math.ceil(Math.random()*moviesList.length)];
-//     movie = moviesList[n];
-//     return movie;
-// }
+
+
+//random elem from array
+function randomMovie(responseContent) {
+    const moviesList = responseContent.results;
+    // const movie = moviesList[Math.ceil(Math.random()*moviesList.length)];
+    movie = moviesList[n];
+    createModal(movie);
+}
+
+//заполнение листа жанров 
+function genreList(genreArray) {
+    genresListM.innerHTML = '';
+    genreArray.forEach((genre) => {
+        for (const genreName in genres) {
+            if (genres[genreName] === genre) {
+                genre = genreName;
+                let li = document.createElement('li');
+                genresListM.appendChild(li);
+                li.innerHTML = genre;
+            }
+        }
+        
+    })
+}
 
 //добавление картинки
-function imgAdd(url, backdrop) {
+function imgAdd(url) {
     let img = document.createElement('img');
     imageContent.appendChild(img);
     img.src = IMG_URL+url;
-    img.dataset.backdrop = IMG_URL+backdrop;
     img.classList.add('w-100');
     img.classList.add('movie-poster');
-
-    //смена картинки при наведении
-    // const changeImage = event => {
-    //     if(event.target.closest('img')) {
-    //         if (backdrop) {
-    //             [img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src]
-    //         }
-    //     }
-    // }
-
-    // img.addEventListener('mouseover', changeImage);
-    // img.addEventListener('mouseout', changeImage);
-
 }
+
+//заполнение окна
+function createModal(movie) {
+    imgAdd(movie.poster_path);
+    genreList(movie.genre_ids);
+    rateM.innerHTML = movie.vote_average;
+    titleM.innerHTML = movie.title;
+    releaseM.innerHTML = movie.release_date;
+    descriptionM.innerHTML = movie.overview;
+    modalOpen();
+}
+
+//message show up
+function messageShowUp() {
+    message.classList.remove('hide');
+}
+
+//плавное исчезновение 
+function messageFadeOut() {
+    message.addEventListener('click', event => {
+        setTimeout(() => message.classList.add('opacity'), 1000);           
+        
+    })
+}
+
+//при нажатии на кнопку Поменять Фильтры
+exitBtns.forEach( (exitBtn) => {
+    exitBtn.addEventListener( 'click', () => {
+        modalClose();
+        message.classList.add('hide');
+        imgRemove();
+    }) 
+})
 
 //удаление картинки 
 function imgRemove() {
@@ -142,7 +166,6 @@ function imgRemove() {
 
 //открытие модального окна
 function modalOpen() {
-    imgRemove();
     modal.classList.remove("hide");
 }
 
@@ -150,3 +173,4 @@ function modalOpen() {
 function modalClose() {
     modal.classList.add("hide");
 }
+
